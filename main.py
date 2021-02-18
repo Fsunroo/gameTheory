9bf48f0634ffc172fb25dded915cdc30d1775115
fsunroo <fsunroo@gmail.com>
2021-02-18 22:47:47 +0330

    Final sols

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
-MAX = 10
+MAX = 100
 CHOICE_RANGE = [i for i in range(MAX)]
 
 
 def Win(STATE):
-    my_choice, op_choice = STATE
-    if my_choice + op_choice <= 10:
-        return my_choice
+    STATE = list(STATE)
+    mean = sum(STATE) / len(STATE)
+    delta = list(map(lambda x: abs(x-(mean/3)), STATE))
+
+    if min(delta) == delta[0]:
+        return 100 / (len(delta)-len(set(delta))+1)
     else:
         return 0
 
@@ -14,12 +17,15 @@ def Win(STATE):
 All_Stats = []
 for me in CHOICE_RANGE:
     result = 0
-    for op in CHOICE_RANGE:
-        result += Win((me, op))
+    for op1 in CHOICE_RANGE:
+        for op2 in CHOICE_RANGE:
+            result += Win((me, op1, op2))
+    All_Stats.append(result)
+
 
 
 All_Stats.append(result)
 plt.plot(All_Stats)
+plt.show()
 
-
-All_Stats.index(max(All_Stats))
+print(All_Stats.index(max(All_Stats)))
